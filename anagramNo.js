737e6c8365fdeb4a3774b6f5f58d124a0c2d8b5d
gooduwill <goodyouwill@gmail.com>
2025-08-24 11:57:32 +0530

    new branch work

--- anagramNo.js
+++ anagramNo.js
@@ -1,4 +1,4 @@
 function anagram(num1,num2){
     return num1.toString().split('').sort().join('')==num2.toString().split('').sort().join('')
 }
-console.log(anagram(342,2165))
+console.log(anagram(342,21654654))
